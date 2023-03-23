7bb4f5f677a8e80ffb88032f91cb16bd2234398f
Jason Weeks <chandlerjweeks0@gmail.com>
2023-03-23 13:36:10 -0500

    Starting models and admin

--- bigMoney/admin.py
+++ bigMoney/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+
+# Cannot be used until models are completed
+"""
+from bigMoney.models import Seller
+from bigMoney.models import Cusomter
+
 
 # Register your models here.
+class SellerInLine(admin.StackedInline):
+    model = Seller
+    can_delete = True
+    verbose_name_plural = "Seller"
+
+class CustomerInLine(admin.StackedInline):
+    model = Cusomter
+    can_delete = True
+    verbose_name_plural = "Customer"
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (SellerInLine,)
+"""
--- bigMoney/models.py
+++ bigMoney/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+#DO NOT migrate models until model design is complete - thank you C:
+"""
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    CompanyName = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    balance = models.FloatField()
+    address = models.ForeignKey(returnAddress, on_delete=models.CASCADE)
+    
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=255)
+    aaddress = models.ForeignKey(shippingAddress, on_delete=models.CASCADE)
+
+class shoppingCart(models.Model):
+    name = models.CharField(max_length=255)
+
+class Order(models.Model):
+    name = models.CharField(max_length=255)
+
+class shippingAddress:
+    RecipiantName = models.CharField(max_length=255)
+    StreetAddress = models.CharField(max_length=255)
+    City = models.CharField(max_length=255)
+    State = models.charField(max_length=20)
+    zipcode = models.intField()
+
+    
+class returnAddress:
+    ShipperName = models.CharField(max_length=255)
+    StreetAddress = models.CharField(max_length=255)
+    City = models.CharField(max_length=255)
+    State = models.charField(max_length=20)
+    zipcode = models.intField()
+
+class merchandise:
+    name = models.CharField(max_length=255)
+
+"""
