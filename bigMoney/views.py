5733210ac120461b6dcd16f2b9e25fe144ba40ee
Guapo Chop <orionkendrick@gmail.com>
2023-03-26 17:23:02 -0500

    Implemented login, reactive header

--- bigMoney/views.py
+++ bigMoney/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
-def login(request):
-    return render(request, "login.html")
+def loginview(request):
+    #return render(request, "login.html")
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
