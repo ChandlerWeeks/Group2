6d5c50ca201acc8f5a4a7093a8f7ba08c6ff4ba6
Guapo Chop <orionkendrick@gmail.com>
2023-04-13 14:28:33 -0500

    some seller tests

--- bigMoney/tests.py
+++ bigMoney/tests.py
@@ -2,13 +2,15 @@ from django.test import Client, TestCase
 from django.urls import reverse
 from .models import User
 from .forms import *
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import merchandise
 
 # Create your tests here.
 
 class RegisterPageTest(TestCase):
     def setUp(self):
             # Set up any necessary data for the test
-            self.url = reverse('register')  # Replace 'register' with the URL name of your register view
+            self.url = reverse('register')
 
     def test_register_page_status_code(self):
             # Test that the register page returns a 200 status code
@@ -72,3 +74,33 @@ class LoginTestCase(TestCase):
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Please enter a correct username and password.')
+
+#seller tests
+class CreateListingTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='testpassword', role='S',email="test@test.com")
+
+    def test_create_listing_view(self):
+        # Log in the test user
+        self.client.login(username='testuser', password='testpassword')
+        
+        # Create a test image file for the form
+        image = SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg")
+
+        # Create a merchandise item data for the form
+        form_data = {
+            'title': 'Test Item',
+            'description': 'This is a test item',
+            'cost': 9.99,
+            'quantity_in_stock': 10,
+            # Add any other required form fields
+        }
+        image = SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg") # Create a test image file for the form
+        # Make a POST request to the create_listing view with the form data and image file
+        response = self.client.post(reverse('create-listing'), data=form_data, files={'image': image})
+
+        self.assertTrue(merchandise.objects.filter(title='Test Item').exists()) # Check that the merchandise item was created in the database
+        self.assertRedirects(response, reverse('home'))# Check that the response redirects to the home view
+
+    
